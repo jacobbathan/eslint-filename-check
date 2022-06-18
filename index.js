ef2138461a7fcf29817591f03f04e1a3b781690c
jacob <jacobathan@gmail.com>
2022-06-17 17:23:26 -0700

    added css/js file testing

--- index.js
+++ index.js
@@ -3,5 +3,7 @@
 module.exports = {
   rules: {
     "jsx-casing": require("./lib/rules/jsx-casing"),
+    "css-casing": require("./lib/rules/css-casing"),
+    "javascript-casing": require("./lib/rules/javascript-casing"),
   },
 };
